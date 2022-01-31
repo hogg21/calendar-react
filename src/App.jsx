462691876cf56acf53c998fa9837c2aa1e6a5b0f
hogg21 <travisjones263@gmail.com>
2022-01-31 17:59:22 +0200

    calendar

--- src/App.jsx
+++ src/App.jsx
@@ -7,17 +7,21 @@ import { getWeekStartDate, generateWeekRange } from '../src/utils/dateUtils.js';
 import './common.scss';
 
 const App = () => {
-
   const [weekStartDate, setWeekStartDate] = useState(getWeekStartDate(new Date()));
-
   const today = () => {
     setWeekStartDate(new Date())
+  }
+  const nextWeek = () => {
+    setWeekStartDate(new Date(weekStartDate.setDate(new Date(weekStartDate).getDate() + 7)))
+  }
+  const prevWeek = () => {
+    setWeekStartDate(new Date(weekStartDate.setDate(new Date(weekStartDate).getDate() - 7)))
   }
   const weekDates = generateWeekRange(getWeekStartDate(weekStartDate));
 
     return (
       <>
-        <Header today={today}/>
+        <Header weekDates={weekDates} today={today} nextWeek={nextWeek} prevWeek={prevWeek}/>
         <Calendar weekDates={weekDates} />
       </>
     );
--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,22 +1,24 @@
 import React from 'react';
-
+import { months } from '../../utils/dateUtils';
 import './header.scss';
 
-const Header = () => {
+const Header = ({ today, weekDates, nextWeek, prevWeek }) => {
+  const week = weekDates.map((dayDate) => dayDate.getMonth());
+  const oneMonth = week[0] === week[6]
   return (
     <header className="header">
       <button className="button create-event-btn">
         <i className="fas fa-plus create-event-btn__icon"></i>Create
       </button>
       <div className="navigation">
-        <button className="navigation__today-btn button">Today</button>
-        <button className="icon-button navigation__nav-icon">
+        <button className="navigation__today-btn button" onClick={today}>Today</button>
+        <button className="icon-button navigation__nav-icon" onClick={prevWeek}>
           <i className="fas fa-chevron-left"></i>
         </button>
-        <button className="icon-button navigation__nav-icon">
+        <button className="icon-button navigation__nav-icon" onClick={nextWeek}>
           <i className="fas fa-chevron-right"></i>
         </button>
-        <span className="navigation__displayed-month"></span>
+        <span className="navigation__displayed-month">{oneMonth ? months[week[0]] : `${months[week[0]]}-${months[week[6]]}`}</span>
       </div>
     </header>
   );
