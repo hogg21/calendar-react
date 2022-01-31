868a88d64bbdfcde68128b2950823a559b9b1784
hogg21 <travisjones263@gmail.com>
2022-01-31 18:07:32 +0200

    nav

--- src/components/navigation/Navigation.jsx
+++ src/components/navigation/Navigation.jsx
@@ -1,3 +1,4 @@
+import moment from 'moment';
 import React from 'react';
 
 import { days } from '../../utils/dateUtils.js';
@@ -5,14 +6,20 @@ import { days } from '../../utils/dateUtils.js';
 const Navigation = ({ weekDates }) => {
   return (
     <header className="calendar__header">
-      {weekDates.map((dayDate, index) => (
-        <div key={index} className="calendar__day-label day-label">
-          <span className="day-label__day-name">{days[dayDate.getDay()]}</span>
-          <span className="day-label__day-number">{dayDate.getDate()}</span>
-        </div>
-      ))}
+      {weekDates.map((dayDate, index) => {
+        let dayWeek = moment(dayDate).format('MMMM Do YYYY') !== moment(new Date()).format('MMMM Do YYYY') ? (
+          { backgroundColor: 'aqua', color: 'white' } ): (
+          { backgroundColor: 'orange', color: 'red' }
+        )
+        return (
+          <div key={index} className="calendar__day-label day-label" style={dayWeek}>
+            <span className="day-label__day-name">{days[dayDate.getDay()]}</span>
+            <span className="day-label__day-number">{dayDate.getDate()}</span>
+          </div>
+        )
+      })}
     </header>
-  );
+  )
 };
 
 export default Navigation;
