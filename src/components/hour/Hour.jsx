bd90c2352f9b8049e856a9adc4a7b1707fb53258
hogg21 <travisjones263@gmail.com>
2022-02-15 17:16:49 +0200

    f

--- src/components/hour/Hour.jsx
+++ src/components/hour/Hour.jsx
@@ -4,10 +4,6 @@ import Event from '../event/Event';
 import { formatMins } from '../../../src/utils/dateUtils.js';
 
 const Hour = ({ dataHour, hourEvents, onCreate, onDelete, events }) => {
-
-  const timeStart = dataHour < 10 ? `0${dataHour}:00` : `${dataHour}:00`;
-  const timeEnd = dataHour < 9 ? `0${dataHour + 1}:00` : `${dataHour + 1}:00`;
-  // debugger;
   return (
     <div className="calendar__time-slot" data-time={dataHour + 1}>
       {/* if no events in the current hour nothing will render here */}
